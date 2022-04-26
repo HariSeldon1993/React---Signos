b49b8a55784fb41d9b60812b8bc41e72a5c08cd2
Henrique Luz <88160031+HariSeldon1993@users.noreply.github.com>
2022-04-26 10:45:56 -0300

    Add files via upload

---
+++ build/precache-manifest.df5eaf34ee4d1d6732d8450e7039b231.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70c826d87b53ce6818d",
+    "url": "/static/css/main.fb630745.chunk.css"
+  },
+  {
+    "revision": "f70c826d87b53ce6818d",
+    "url": "/static/js/main.f70c826d.chunk.js"
+  },
+  {
+    "revision": "f993ed0ea40465f97e7e",
+    "url": "/static/js/1.f993ed0e.chunk.js"
+  },
+  {
+    "revision": "0d529ed659e39574b722",
+    "url": "/static/js/2.0d529ed6.chunk.js"
+  },
+  {
+    "revision": "d895deb6de546ffabfe8",
+    "url": "/static/js/runtime~main.d895deb6.js"
+  },
+  {
+    "revision": "845eb3101a7e52c5d4a3e05589d24904",
+    "url": "/static/media/Aries.845eb310.jpg"
+  },
+  {
+    "revision": "bfa9208f9439482fccbb785c554762e2",
+    "url": "/static/media/Touro.bfa9208f.jpg"
+  },
+  {
+    "revision": "4835a8e406eedee38b1cf20f3317865e",
+    "url": "/static/media/Gemeos.4835a8e4.jpg"
+  },
+  {
+    "revision": "1492ff58c7ed1e52e56ab4e47cc02fe6",
+    "url": "/static/media/Cancer.1492ff58.jpg"
+  },
+  {
+    "revision": "5fe348fe6e29152677043190fa8e0fed",
+    "url": "/static/media/Leao.5fe348fe.jpg"
+  },
+  {
+    "revision": "334bf204d1d40317cc4c159f0e6aef8d",
+    "url": "/static/media/Virgem.334bf204.jpg"
+  },
+  {
+    "revision": "8590c3a51f08e4b577db137ce9c6d889",
+    "url": "/static/media/Libra.8590c3a5.jpg"
+  },
+  {
+    "revision": "4f1187570c49d1c7786313e13f9b8128",
+    "url": "/static/media/Escorpiao.4f118757.jpg"
+  },
+  {
+    "revision": "383500a1ce892a1c604338361d546720",
+    "url": "/static/media/Sagitario.383500a1.jpg"
+  },
+  {
+    "revision": "f72319a2a592a465d38e5c345dc7f997",
+    "url": "/static/media/Capricornio.f72319a2.jpg"
+  },
+  {
+    "revision": "de1fdae86e9d43813ca39fad8292cdf6",
+    "url": "/static/media/Aquario.de1fdae8.jpg"
+  },
+  {
+    "revision": "1bf0623a19ee824a31a457b31166110f",
+    "url": "/static/media/Peixes.1bf0623a.jpg"
+  },
+  {
+    "revision": "41c754e08e62b8a0496eb15fe138d786",
+    "url": "/index.html"
+  }
+];
